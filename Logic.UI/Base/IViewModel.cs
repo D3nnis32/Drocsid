fc3069778fcd8b3b2f5665b4be2b601e909632fb
Henrik Groke <120175080+Puddingel@users.noreply.github.com>
2025-03-06 10:24:41 +0100

    Updated View and added basic MVVM Architecture
    MVVM architecture atm includes:
     - View
     - Logic.UI
     - Services (empty)
    
    Updated UI with seperated Windows and UserControls for better control and to prepare dynamic instantiation

---
+++ Logic.UI/Base/IViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.UI.Base
+{
+    public interface IViewModel<TypeOfModel> : INotifyPropertyChanged
+    {
+        TypeOfModel Model { get; set; }
+
+        void NewModelAssigned();
+    }
+}
---
+++ Logic.UI/ViewModelLocator.cs
@@ -0,0 +1,24 @@
+﻿using Logic.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.UI
+{
+    public class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowVM { get; set; }
+        public ApplicationUserControlViewModel ApplicationUCVM { get; set; }
+        public LoginUserControlViewModel LoginUCVM { get; set; }
+        public ChatUserControlViewModel ChatUCVM { get; set; }
+        public ViewModelLocator()
+        {
+            MainWindowVM = new MainWindowViewModel();
+            ApplicationUCVM = new ApplicationUserControlViewModel();
+            LoginUCVM = new LoginUserControlViewModel();
+            ChatUCVM = new ChatUserControlViewModel();
+        }
+    }
+}
