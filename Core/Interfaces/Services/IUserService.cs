78599d37c5af13a20a8e43881786b6f3097dc521
Henrik Groke <120175080+Puddingel@users.noreply.github.com>
2025-03-06 11:52:03 +0100

    Interface/Services Reintroduced

--- Core/Interfaces/Services/IUserService.cs
+++ Core/Interfaces/Services/IUserService.cs
@@ -1,4 +1,3 @@
-using System.Linq.Expressions;
 using Drocsid.HenrikDennis2025.Core.Models;
 
 namespace Drocsid.HenrikDennis2025.Core.Interfaces.Services;
@@ -7,7 +6,6 @@ public interface IUserService
 {
     Task<User> GetUserByIdAsync(Guid userId);
     Task<IEnumerable<User>> GetAllUsersAsync();
-    Task<IEnumerable<User>> FindUsersAsync(Expression<Func<User, bool>> predicate);
     Task<User> CreateUserAsync(User user, string password);
     Task UpdateUserAsync(User user);
     Task UpdateUserStatusAsync(Guid userId, UserStatus status);
