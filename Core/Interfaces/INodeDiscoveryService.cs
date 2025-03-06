6056219cc6ba20b4097944b486b08f2f8c18d393
Henrik Groke <120175080+Puddingel@users.noreply.github.com>
2025-03-06 10:50:25 +0100

    Revert "Merge branch 'master' of https://github.com/D3nnis32/Drocsid"
    This reverts commit 8c93f66aaa5262944f0cac1f6a6659a378215a69, reversing
    changes made to fc3069778fcd8b3b2f5665b4be2b601e909632fb.

--- Core/Interfaces/INodeDiscoveryService.cs
+++
@@ -1,9 +0,0 @@
-namespace Drocsid.HenrikDennis2025.Core.Interfaces;
-
-public interface INodeDiscoveryService
-{
-    Task RegisterNodeAsync(string nodeId, string endpoint);
-    Task UnregisterNodeAsync(string nodeId);
-    Task<IEnumerable<NodeInfo>> GetActiveNodesAsync();
-    Task ReportHealthStatusAsync(string nodeId, bool isHealthy);
-}
